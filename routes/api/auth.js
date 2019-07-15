8d900c64ef5752c4bd7c2f6291a38566062bf534
Sabarish <k.sabarish@milvik.se>
2019-07-15 16:38:46 +0530

    User JWT Parses with middleware done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,13 +2,25 @@
 const express = require('express');
 // Useexpress Reouter
 const router = express.Router();
+// get hte middleware
 
+const auth = require('../../middleware/auth');
+// NOTE : add to second parameter to route to protect the route, and apply middleware
+
+const User = require('../../models/Users');
 // @route  GET api/auth
 // @desc   Test Route
 // @access Public
-router.get('/', (req,res) =>{
-    res.send('Auth Route');
-})
+router.get('/', auth, async (req,res) =>{
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 // Export the router
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const {check, validationResult} = require('express-validator');
+
 
 const app = express();
 
