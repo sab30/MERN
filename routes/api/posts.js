9943b79c83a221c2f20290ce3fff20fae7589746
Sabarish <k.sabarish@milvik.se>
2019-09-02 14:05:28 +0530

    Posts API Add

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,45 @@
-// User Js
 const express = require('express');
-// Useexpress Reouter
+const config = require('config');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
-// @route  GET api/posts
-// @desc   Test Route
+// @route  POST api/posts
+// @desc   Create a  POST
 // @access Public
-router.get('/', (req,res) =>{
-    res.send('Posts Route');
+router.post('/',[
+    auth ,
+    [
+        check('text', "Text is required").not().isEmpty()
+    ]
+] , async (req,res) =>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text : req.body.text,
+            name : user.name,
+            avatar : user.avatar,
+            user : req.user.id
+        });
+
+        const post = await newPost.save();
+
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+
 })
 
 // Export the router
